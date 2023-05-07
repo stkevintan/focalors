03595c24c14859d0b8b5400ba083991282d15494
stkevintan <stkevintan@zju.edu.cn>
2023-05-07 17:58:53 +0800

    init

--- packages/yunzai-client/src/index.ts
+++
@@ -1,7 +0,0 @@
-import "reflect-metadata";
-
-export { YunzaiClient } from "./client";
-
-export * from "./tokens";
-export * from "./types";
-export * from "./config";
--- packages/yunzai-client/src/logger.ts
+++
@@ -1,7 +0,0 @@
-import path from "path";
-import { createLogger } from "@focalors/logger";
-
-export const logger = createLogger({
-    name: "yunzai-client",
-    filename: path.resolve(__dirname, "../logs/stdout"),
-});
