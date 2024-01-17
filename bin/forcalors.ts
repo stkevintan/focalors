1b7bf629f390cffd921bfce85b94181163c92f7b
stkevintan <stkevintan@zju.edu.cn>
2024-01-18 00:44:04 +0800

    use local pm2

--- bin/forcalors.ts
+++ bin/forcalors.ts
@@ -25,10 +25,11 @@ async function main() {
             GPTClient
         );
         await program.start();
+        process.send?.('ready');
 
         async function exitHandler() {
             logger.info("\nGracefully shutting down...");
-            await program.stop().finally(() => process.exit());
+            await program.stop().finally(() => process.exit(0));
         }
 
         // catches ctrl+c event
@@ -41,7 +42,14 @@ async function main() {
         // catches uncaught exceptions
         process.on("uncaughtException", (e) => {
             logger.error("Uncaught exception:", e);
-            exitHandler();
+            void exitHandler();
+        });
+
+        // Windows graceful stop
+        process.on("message", function (msg) {
+            if (msg == "shutdown") {
+                void exitHandler();
+            }
         });
     } catch (err) {
         logger.error(err);
