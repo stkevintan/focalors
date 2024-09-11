6e10fa02b9901462a6545adf749a64d7c4547bdc
stkevintan <stkevintan@zju.edu.cn>
2024-09-11 14:49:48 +0800

    add bold util

--- packages/custom-client/src/jandan.ts
+++ packages/custom-client/src/jandan.ts
@@ -12,6 +12,7 @@ import {
     TextMessageSegment,
 } from "@focalors/onebot-protocol";
 import { inject, injectable } from "tsyringe";
+import { bold } from "./utils/bold";
 
 const logger = createLogger("jandan-client");
 
@@ -44,6 +45,9 @@ enum JandanTimerStatus {
     stopped,
 }
 
+
+const oo = bold('oo');
+const xx = bold('xx');
 @injectable()
 export class JanDanClient extends OnebotClient {
     constructor(
@@ -80,7 +84,10 @@ export class JanDanClient extends OnebotClient {
 
         if (matchRet) {
             const top = Number.parseInt(matchRet[2], 10) || 1;
-            await this.sendJandan(from, top);
+            const cnt = await this.sendJandan(from, top);
+            if (cnt === 0) {
+                this.sendText("暂无更新, 请稍候再试...", from);
+            }
             return true;
         }
 
@@ -92,34 +99,34 @@ export class JanDanClient extends OnebotClient {
         const id = groupId ?? userId!;
 
         if (/^#\s*煎蛋开启定时转发\s*$/i.test(text)) {
-            const status = (await this.redis.hGet(timerKey, id)) ?? 0;
-            if (status === JandanTimerStatus.on) {
-                this.sendText("已开启", from);
-                return true;
-            }
+            // const status = (await this.redis.hGet(timerKey, id)) ?? 0;
+            // if (status === JandanTimerStatus.on) {
+            //     this.sendText("已开启", from);
+            //     return true;
+            // }
 
-            await this.redis.hSet(timerKey, id, JandanTimerStatus.on);
+            // await this.redis.hSet(timerKey, id, JandanTimerStatus.on);
 
-            if (status === JandanTimerStatus.stopped) {
-                this.sendText("已开启", from);
-                return true;
-            }
+            // if (status === JandanTimerStatus.stopped) {
+            //     this.sendText("已开启", from);
+            //     return true;
+            // }
 
-            void (async () => {
-                for (;;) {
-                    await this.sendJandan(from);
-                    await new Promise((r) =>
-                        setTimeout(r, 1000 * 60 * 60 * 60)
-                    );
-                    const currentStatus = await this.redis.hGet(timerKey, id);
-                    if (JandanTimerStatus.stopped === currentStatus) {
-                        await this.redis.hDel(timerKey, id);
-                    }
-                    if (JandanTimerStatus.on !== currentStatus) {
-                        break;
-                    }
-                }
-            })();
+            // void (async () => {
+            //     for (;;) {
+            //         await this.sendJandan(from);
+            //         await new Promise((r) =>
+            //             setTimeout(r, 1000 * 60 * 60 * 60)
+            //         );
+            //         const currentStatus = await this.redis.hGet(timerKey, id);
+            //         if (JandanTimerStatus.stopped === currentStatus) {
+            //             await this.redis.hDel(timerKey, id);
+            //         }
+            //         if (JandanTimerStatus.on !== currentStatus) {
+            //             break;
+            //         }
+            //     }
+            // })();
             return true;
         }
 
@@ -140,13 +147,13 @@ export class JanDanClient extends OnebotClient {
 
     private async sendJandan(from: MessageTarget2, top = 1) {
         const commentUrl = `https://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments`;
+        let cnt = 0;
         try {
             const resp: { comments: Comment[] } = await fetch(commentUrl, {
                 headers,
             }).then((r) => r.json());
             const { userId, groupId } = expandTarget(from);
             const key = this.key(groupId ?? userId!);
-            let cnt = 0;
             for (const comment of resp.comments) {
                 if (await this.redis.sIn(key, comment.comment_ID)) {
                     continue;
@@ -167,8 +174,10 @@ export class JanDanClient extends OnebotClient {
                 const tucao = await this.getSubComments(comment);
                 this.sendText(
                     [
-                        `${comment.text_content} 作者: ${comment.comment_author}`,
-                        `oo: ${comment.vote_positive}, xx: ${comment.vote_negative}`,
+                        `${comment.text_content?.trim()} 作者: ${
+                            comment.comment_author
+                        }`,
+                        `${oo}: ${comment.vote_positive}, ${xx}: ${comment.vote_negative}`,
                         tucao,
                     ]
                         .join("\n")
@@ -182,14 +191,10 @@ export class JanDanClient extends OnebotClient {
                     break;
                 }
             }
-
-            if (cnt === 0) {
-                this.sendText("暂无更新, 请稍候再试...", from);
-            }
         } catch (err) {
             logger.error(err);
-            this.sendText("糟糕，煎蛋获取失败", from);
         }
+        return cnt;
     }
 
     private async getSubComments(comment: Comment) {
@@ -203,7 +208,7 @@ export class JanDanClient extends OnebotClient {
             ).then((r) => r.json());
             const list = resp.data.list as SubComment[];
             return list
-                .map((comment) => `**${comment.author}**: ${comment.content}`)
+                .map((comment) => `${bold(comment.author)}: ${comment.content}`)
                 .join("\n");
         } catch (err) {
             logger.error(err);
@@ -211,3 +216,4 @@ export class JanDanClient extends OnebotClient {
         }
     }
 }
+
