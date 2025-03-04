c9fc5c54b876fc009e42ba5a907640ebba1b0f44
stkevintan <stkevintan@zju.edu.cn>
2025-03-04 08:56:57 +0800

    udpate version of nodejs

--- docker/yunzai/entrypoint.sh
+++ docker/yunzai/entrypoint.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
+# Disable exit-on-error behavior
 set +e
 
-GreenBG="\\033[42;37m"
-YellowBG="\\033[43;37m"
-BlueBG="\\033[44;37m"
-Font="\\033[0m"
+GreenBG="\033[42;37m"
+YellowBG="\033[43;37m"
+BlueBG="\033[44;37m"
+Font="\033[0m"
 
 Version="${BlueBG}[版本]${Font}"
 Info="${GreenBG}[信息]${Font}"
@@ -13,65 +14,71 @@ Warn="${YellowBG}[提示]${Font}"
 
 WORK_DIR="/app/Miao-Yunzai"
 
-if [[ ! -d "$HOME/.ovo" ]]; then
-    mkdir ~/.ovo
+if [ ! -d "$HOME/.ovo" ]; then
+    mkdir -p "$HOME/.ovo"
 fi
 
-
-function update_yunzai() {
-    echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 Miao-Yunzai 更新 ${Font} \n ================ \n"
-    cd $WORK_DIR
-    if [[ -z $(git status -s) ]]; then
+update_yunzai() {
+    printf "%b\n" "\n ================ \n ${Info} ${GreenBG} 拉取 Miao-Yunzai 更新 ${Font} \n ================ \n"
+    cd "$WORK_DIR" || exit 1
+    if [ -z "$(git status -s)" ]; then
         git reset --hard HEAD
         git pull --allow-unrelated-histories
     else
         git pull --allow-unrelated-histories
     fi
-    if [[ ! -f "$HOME/.ovo/yunzai.ok" ]]; then
+    if [ ! -f "$HOME/.ovo/yunzai.ok" ]; then
         set -e
-        echo -e "\n ================ \n ${Info} ${GreenBG} 更新 Miao-Yunzai 运行依赖 ${Font} \n ================ \n"
+        printf "%b\n" "\n ================ \n ${Info} ${GreenBG} 更新 Miao-Yunzai 运行依赖 ${Font} \n ================ \n"
         pnpm install -P
-        touch ~/.ovo/yunzai.ok
+        touch "$HOME/.ovo/yunzai.ok"
         set +e
     fi
-    echo -e "\n ================ \n ${Version} ${BlueBG} Miao-Yunzai 版本信息 ${Font} \n ================ \n"
+    printf "%b\n" "\n ================ \n ${Version} ${BlueBG} Miao-Yunzai 版本信息 ${Font} \n ================ \n"
     git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
 }
 
 sync_plugin() {
-    local repo_url=$1
-    local plugin_name=$2
-    local clone_if_not_exists=${3:-false}
-    local plugin_root="$WORK_DIR/plugins/$plugin_name"
+    repo_url="$1"
+    plugin_name="$2"
+    # Use the default value if not provided
+    if [ -z "$3" ]; then
+        clone_if_not_exists="false"
+    else
+        clone_if_not_exists="$3"
+    fi
+    plugin_root="$WORK_DIR/plugins/$plugin_name"
     if [ -d "$plugin_root/.git" ]; then
-        echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 ${plugin_name} 更新 ${Font} \n ================ \n"
-        cd "$plugin_root"
-        if [[ -n $(git status -s) ]]; then
+        printf "%b\n" "\n ================ \n ${Info} ${GreenBG} 拉取 ${plugin_name} 更新 ${Font} \n ================ \n"
+        cd "$plugin_root" || exit 1
+        if [ -n "$(git status -s)" ]; then
             git reset --hard HEAD
         fi
         git pull --allow-unrelated-histories
     elif [ "$clone_if_not_exists" = "true" ]; then
-        echo -e "\n ${Warn} ${YellowBG} 由于喵版云崽依赖${plugin_name}，检测到目前没有安装，开始自动下载 ${Font} \n"
+        printf "%b\n" "\n ${Warn} ${YellowBG} 由于喵版云崽依赖${plugin_name}，检测到目前没有安装，开始自动下载 ${Font} \n"
         git clone --depth=1 "$repo_url" "$plugin_root"
     fi
 }
 
 update_yunzai
 sync_plugin "https://gitee.com/TimeRainStarSky/Yunzai-genshin" "genshin" true
-sync_plugin "https://gitee.com/yoimiya-kokomi/miao-plugin" "miao-plugin" true
+sync_plugin "https://github.com/yoimiya-kokomi/miao-plugin" "miao-plugin" true
 sync_plugin "https://gitee.com/Ctrlcvs/xiaoyao-cvs-plugin" "xiaoyao-cvs-plugin" true
+pnpm i -P
 
 set -e
 
-cd $WORK_DIR
+cd "$WORK_DIR" || exit 1
 
-echo -e "\n ================ \n ${Info} ${GreenBG} 初始化 Docker 环境 ${Font} \n ================ \n"
+printf "%b\n" "\n ================ \n ${Info} ${GreenBG} 初始化 Docker 环境 ${Font} \n ================ \n"
 
 if [ -f "./config/config/redis.yaml" ]; then
     sed -i 's/127.0.0.1/miao-redis/g' ./config/config/redis.yaml
-    echo -e "\n  修改Redis地址完成~  \n"
+    printf "%b\n" "\n  修改Redis地址完成~  \n"
 fi
 
-echo -e "\n ================ \n ${Info} ${GreenBG} 启动 Miao-Yunzai ${Font} \n ================ \n"
+printf "%b\n" "\n ================ \n ${Info} ${GreenBG} 启动 Miao-Yunzai ${Font} \n ================ \n"
+
 
 node app
